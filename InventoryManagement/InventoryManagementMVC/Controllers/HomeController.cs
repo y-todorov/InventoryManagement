f29411b004a6bb3a456ca7a6f119e563a3fbdb33
y-todorov <ytodorov@ytodorov.com>
2013-10-23 23:38:00 +0300

    This is the first commit!!!

---
+++ InventoryManagement/InventoryManagementMVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace KendoUIMvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
