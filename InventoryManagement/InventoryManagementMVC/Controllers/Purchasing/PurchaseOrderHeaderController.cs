8c471746c7d2667c3a63f739fc0bf43e8a25ed12
y-todorov <ytodorov@ytodorov.com>
2013-11-01 11:12:15 +0200

    PurchaseOrderHeaderViewModel

---
+++ InventoryManagement/InventoryManagementMVC/Controllers/Purchasing/PurchaseOrderHeaderController.cs
@@ -0,0 +1,36 @@
+﻿using InventoryManagementMVC.Models.Purchasing;
+using Kendo.Mvc.UI;
+using RecipiesModelNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+
+namespace InventoryManagementMVC.Controllers.Purchasing
+{
+    public class PurchaseOrderHeaderController : Controller
+    {
+        //
+        // GET: /PurchaseOrder/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Read([DataSourceRequest] DataSourceRequest request)
+        {
+            List<PurchaseOrderHeaderViewModel> purchaseOrderHeaderViewModels =
+                ContextFactory.Current.PurchaseOrderHeaders
+                //.Include(pod => pod.PurchaseOrderHeader)
+                //.Include(pod => pod.Product)
+                //.Include(pod => pod.Product.ProductCategory)
+                .ToList().Select
+                (pod => PurchaseOrderHeaderViewModel.ConvertFromPurchaseOrderHeaderEntity(pod, new PurchaseOrderHeaderViewModel())).ToList();
+            return Json(purchaseOrderHeaderViewModels.ToDataSourceResult(request));
+        }
+
+    }
+}
